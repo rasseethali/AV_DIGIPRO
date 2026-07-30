19ca9e185725d26dd360df5c9e77882e7d7377ad
rasseethali <raseethali46gmail.com>
2026-07-30 12:46:17 +0530

    updated smtp configuration

--- Backend/controllers/enquiryController.js
+++ Backend/controllers/enquiryController.js
@@ -9,9 +9,6 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
 });
 // Verify SMTP Connection
 transporter.verify((error, success) => {
