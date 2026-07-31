6182a34c466ac5344d59a1e72d6bc52ed76f8e1a
rasseethali <raseethali46gmail.com>
2026-07-31 16:45:41 +0530

    Backend localhost

--- Backend/routes/enquiryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { sendEnquiry } = require("../controllers/enquiryController");
-
-router.post("/", sendEnquiry);
-
-module.exports = router;
