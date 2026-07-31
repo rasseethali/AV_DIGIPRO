55b2e84f16f95e374af51e36b79077013f745002
rasseethali <raseethali46gmail.com>
2026-07-31 11:53:32 +0530

    test3

--- Backend/controllers/enquiryController.js
+++ Backend/controllers/enquiryController.js
@@ -57,22 +57,16 @@ const sendEnquiry = async (req, res) => {
     // ===========================
     const ownerMail = {
       from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER, // Owner Gmail
-      replyTo: email,
-      subject: "📩 New AV DIGIPRO Enquiry",
-
-      html: `
-      <h2>New Enquiry Received</h2>
-
-      <table border="1" cellpadding="10" cellspacing="0" style="border-collapse:collapse;">
-        <tr><td><b>Name</b></td><td>${name}</td></tr>
-        <tr><td><b>Phone</b></td><td>${phone}</td></tr>
-        <tr><td><b>Email</b></td><td>${email}</td></tr>
-        <tr><td><b>State</b></td><td>${state}</td></tr>
-        <tr><td><b>Language</b></td><td>${language}</td></tr>
-        <tr><td><b>Message</b></td><td>${message}</td></tr>
-      </table>
-      `,
+      to: process.env.EMAIL_USER,
+      subject: "SMTP Test from Enquiry",
+      text: `
+Name: ${name}
+Phone: ${phone}
+Email: ${email}
+State: ${state}
+Language: ${language}
+Message: ${message}
+  `,
     };
 
     // ===========================
@@ -123,15 +117,15 @@ const sendEnquiry = async (req, res) => {
       `,
     };
     console.log("➡️ Sending Owner Mail...");
-await transporter.sendMail(ownerMail);
-console.log("✅ Owner Mail Sent");
+    await transporter.sendMail(ownerMail);
+    console.log("✅ Owner Mail Sent");
 
-// Temporary disable this
-console.log("➡️ Sending User Mail...");
-await transporter.sendMail(userMail);
-console.log("✅ User Mail Sent");
+    // Temporary disable this
+    console.log("➡️ Sending User Mail...");
+    await transporter.sendMail(userMail);
+    console.log("✅ User Mail Sent");
 
-  
+
 
     return res.status(200).json({
       success: true,
